628d1ef6d8865f2fb6362ece2fad9e7c1b07dba5
Graypbj <graypbj@gmail.com>
2025-11-14 09:38:14 -0700

    Sync for other computers

--- main.go
+++ main.go
@@ -1,42 +1,33 @@
 package main
 
 import (
+	"image/color"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
-// Game struct holds all your game's data.
 type Game struct{}
 
-// Update is called every frame to update game logic.
 func (g *Game) Update() error {
-	// For now, we'll leave this empty.
 	return nil
 }
 
-// Draw is called every frame to draw the game screen.
 func (g *Game) Draw(screen *ebiten.Image) {
-	// Draw a "Hello, World!" message.
+	screen.Fill(color.RGBA{120, 180, 255, 255})
 	ebitenutil.DebugPrint(screen, "Hello, World!")
 }
 
-// Layout defines the game's window size.
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 640, 480
+	return 320, 240
 }
 
 func main() {
-	// Create a new Game object.
-	game := &Game{}
-
-	// Set the window size and title.
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("My Go Game")
-
-	// Run the game!
-	if err := ebiten.RunGame(game); err != nil {
+	ebiten.SetWindowTitle("Hello, World!")
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	if err := ebiten.RunGame(&Game{}); err != nil {
 		log.Fatal(err)
 	}
 }
